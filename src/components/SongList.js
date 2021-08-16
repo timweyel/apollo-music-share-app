6a01d4dd58f9dad138b7f86ce9f6feca6f1ac5ad
TimWeyel <tweyel@gmail.com>
2021-08-16 14:25:59 -0700

    added songlist component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,96 @@
+import { 
+  CardContent, 
+  CardMedia, 
+  CircularProgress, 
+  IconButton, 
+  Typography,
+  Card,
+  CardActions,
+  makeStyles } from '@material-ui/core';
+import { PlayArrow, Save } from '@material-ui/icons';
 import React from 'react';
 
 function SongList() {
-  return <div>song list</div>
-}
+  let loading = false;
+
+  const song = {
+    title: "10th Avenue Freezeout",
+    artist: "Springsteen",
+    thumbnail: "https://cdn.mos.cms.futurecdn.net/EjnCZRe2ZLzr8v46LZXdGH-970-80.jpg.webp"
+  }
+
+  if (loading) {
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: 50
+      }}>
+        <CircularProgress />
+      </div>
+    )
+  }
+  
+  return (
+    <div>
+      {Array.from({ length: 10 }, () => song).map((song, i) => (
+        <Song key={i} song={song}/>
+      ))}
+    </div>
+    );
+  }
+
+  const useStyles = makeStyles(theme => ({
+    container: {
+      margin: theme.spacing(3)
+    },
+    songInfoContainer: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    songInfo: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'space-between'
+    },
+    thumbnail: {
+      objectFit: 'cover',
+      width: 140,
+      height: 140
+    }
+  }))
+
+  function Song({ song }) {
+    const classes = useStyles();
+    const { title, artist, thumbnail } = song
+    
+    return (
+    <Card className={classes.container}>
+      <div className={classes.songInfoContainer}>
+        <CardMedia image={thumbnail} className={classes.thumbnail}/>
+        <div className={classes.songInfo}>
+          <CardContent>
+            <Typography gutterBottom variant = "h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant = "body1" component="p" color="textSecondary">
+              {artist}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton size="small" color="primary">
+              <PlayArrow />
+            </IconButton>
+            <IconButton size="small" color="secondary">
+              <Save />
+            </IconButton>
+          </CardActions>
+        </div>
+      </div>
+    </Card>
+    );
+  }
+  
 
 export default SongList;
