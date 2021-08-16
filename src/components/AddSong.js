1ef27538dd651e333622b081110904a635d23598
TimWeyel <tweyel@gmail.com>
2021-08-16 11:49:57 -0700

    added URL element and add button

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { TextField, InputAdornment, Button } from "@material-ui/core";
+import { Link, AddBoxOutlined } from '@material-ui/icons'
 
 function AddSong() {
-  return <div>add song</div>
+  return (
+    <div>
+      <TextField
+        placeholder = "Add Youtube or Soundcloud Url"
+        fullWidth
+        margin = "normal"
+        type= "url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment>
+              <Link />
+            </InputAdornment>
+          )
+        }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+      >
+        Add
+      </Button>
+    </div>
+  )
 }
 
 export default AddSong;
