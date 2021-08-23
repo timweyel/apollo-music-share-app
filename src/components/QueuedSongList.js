82204309c02e1d369c80232cb7ce692882306f76
TimWeyel <tweyel@gmail.com>
2021-08-23 12:37:54 -0700

    code cleanup

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,8 +1,37 @@
-import { useMutation } from '@apollo/client';
-import { Avatar, IconButton, Typography, makeStyles, useMediaQuery } from '@material-ui/core';
-import { Delete } from '@material-ui/icons';
-import React from 'react';
-import { ADD_OR_REMOVE_FROM_QUEUE } from '../graphql/mutations';
+import React from "react";
+import {
+  Typography,
+  Avatar,
+  IconButton,
+  makeStyles,
+  useMediaQuery
+} from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
+import { useMutation } from "@apollo/react-hooks";
+import { ADD_OR_REMOVE_FROM_QUEUE } from "../graphql/mutations";
+
+function QueuedSongList({ queue }) {
+  const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up("md"));
+
+  // const song = {
+  //   title: "LÜNE",
+  //   artist: "MÖÖN",
+  //   thumbnail: "http://img.youtube.com/vi/--ZtUFsIgMk/0.jpg"
+  // };
+
+  return (
+    greaterThanMd && (
+      <div style={{ margin: "10px 0" }}>
+        <Typography color="textSecondary" variant="button">
+          QUEUE ({queue.length})
+        </Typography>
+        {queue.map(song => (
+          <QueuedSong key={song.id} song={song} />
+        ))}
+      </div>
+    )
+  );
+}
 
 const useStyles = makeStyles({
   avatar: {
@@ -10,70 +39,50 @@ const useStyles = makeStyles({
     height: 44
   },
   text: {
-    textOverflow: 'ellipsis',
-    overflow: 'hidden'
+    textOverflow: "ellipsis",
+    overflow: "hidden"
   },
   container: {
-    display: 'grid',
-    gridAutoFlow: 'column',
-    gridTemplateColumns: '50px auto 50px',
+    display: "grid",
+    gridAutoFlow: "column",
+    gridTemplateColumns: "50px auto 50px",
     gridGap: 12,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10
   },
   songInfoContainer: {
-    overflow: 'hidden',
-    whiteSpace: 'nowrap'
+    overflow: "hidden",
+    whiteSpace: "nowrap"
   }
 });
 
-function QueuedSongList({ queue }) {
-  console.log({ queue });
-  const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up('md'));
-
-  // const song = {
-  //   title: "Tenth Avenue Freeze Out",
-  //   artist: "Springsteen",
-  //   thumbnail: "https://cdn.mos.cms.futurecdn.net/EjnCZRe2ZLzr8v46LZXdGH-970-80.jpg.webp"
-  // }
-
-  return ( 
-    greaterThanMd && ( 
-    <div style={{ margin: '10px 0' }}>
-      <Typography color="textSecondary" variant="button">
-        QUEUE ({queue.length})
-      </Typography>
-      {queue.map((song, i) => (
-        <QueuedSong key={i} song={song} />
-      ))}
-    </div>
-  )
-);
-}
-
 function QueuedSong({ song }) {
-  const classes = useStyles();  
-  const { thumbnail, artist, title } = song;
+  const classes = useStyles();
   const [addOrRemoveFromQueue] = useMutation(ADD_OR_REMOVE_FROM_QUEUE, {
     onCompleted: data => {
-      localStorage.setItem('queue', JSON.stringify(data.addOrRemoveFromQueue))
+      localStorage.setItem("queue", JSON.stringify(data.addOrRemoveFromQueue));
     }
   });
+  const { thumbnail, artist, title } = song;
 
   function handleAddOrRemoveFromQueue() {
     addOrRemoveFromQueue({
-      variables: { input: { ...song, __typename: 'Song' }}
-    })
+      variables: { input: { ...song, __typename: "Song" } }
+    });
   }
 
   return (
     <div className={classes.container}>
-      <Avatar src={thumbnail} alt="song thumbnail" className={classes.avatar} />
+      <Avatar src={thumbnail} alt="Song thumbnail" className={classes.avatar} />
       <div className={classes.songInfoContainer}>
         <Typography variant="subtitle2" className={classes.text}>
           {title}
         </Typography>
-        <Typography color="textSecondary" variant="body2" className={classes.text}>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.text}
+        >
           {artist}
         </Typography>
       </div>
@@ -81,7 +90,7 @@ function QueuedSong({ song }) {
         <Delete color="error" />
       </IconButton>
     </div>
-  )
+  );
 }
 
 export default QueuedSongList;
